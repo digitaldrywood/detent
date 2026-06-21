b08d247ba4217eb30664ffb2b615a9c1284dcc7b
Cory LaNou <cory@lanou.com>
2026-06-21 07:29:33 -0500

    fix(install): avoid forwarding tokens on redirects

--- install.sh
+++ install.sh
@@ -96,7 +96,7 @@ github_api_token() {
 download_api_file() {
 	token="$(github_api_token)"
 	if [ -n "$token" ]; then
-		curl -fsSL \
+		curl -fsS \
 			-H "Accept: application/vnd.github+json" \
 			-H "Authorization: Bearer $token" \
 			-H "X-GitHub-Api-Version: 2022-11-28" \
